b932a653971ec825357df35069a38a56e8d85a8b
kevin <keebvhev@gmail.com>
2019-09-27 16:59:25 +0800

    build frontpage: /restaurants

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express')
-const handelebars = require('express-handlebars')
+const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
 
-app.engine('handlebars', handelebars())
+app.engine('handlebars', handlebars({ defaultLayout: 'main' })) //handlebars v3.1.0 優化中已直接帶入，故可不寫
 app.set('view engine', 'handlebars')
 
 
@@ -11,3 +11,9 @@ app.listen(port, () => {
   // console.log("example app listen on port:" + port)
   console.log(`example app listen on portxx: ${port}`)
 })
+
+// 引入 routes 並將 app 傳進去，讓 routes 可以用 app 這個物件來指定路由
+require('./routes')(app) //需要放在 app.js 的最後一行
+
+
+const exphbs = require('express-handlebars')
