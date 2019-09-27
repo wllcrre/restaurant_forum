884616c40f4fec1015f1c8583f44e1354f898488
kevin <keebvhev@gmail.com>
2019-09-27 17:05:24 +0800

    add /admin page

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 const restController = require('../controllers/restController.js')
+const adminController = require('../controllers/adminController.js')
+
 module.exports = app => { // 此箭頭函式中 app 是一個參數
 
-  //如果使用者訪問首頁，就導向 /restaurants 的頁面
   app.get('/', (req, res) => res.redirect('/restaurants'))
+  app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
 
   //在 /restaurants 底下則交給 restController.getRestaurants 來處理
   app.get('/restaurants', restController.getRestaurants)
+
+  // 在 /admin/restaurants 底下則交給 adminController.getRestaurants 處理
+  app.get('/admin/restaurants', adminController.getRestaurants)
 }
 
