e541d0d1e9496db39e0ff952fa3a5a92b192a54f
kevin <keebvhev@gmail.com>
2019-10-06 16:42:21 +0800

    user register process

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+
 const db = require('./models') // 引入資料庫
+const bodyParser = require('body-parser') // for http POST, req.body 
 const app = express()
 const port = 3000
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' })) //handlebars v3.1.0 優化中已直接帶入，故可不寫
 app.set('view engine', 'handlebars')
-
+app.use(bodyParser.urlencoded({ extended: true }))// for http POST, req.body //app.use: 所有的請求都會先被 bodyParser 進行處理
 
 app.listen(port, () => {
   // console.log("example app listen on port:" + port)
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const restController = require('../controllers/restController.js')
 const adminController = require('../controllers/adminController.js')
+const userController = require('../controllers/userController.js')
 
 module.exports = app => { // 此箭頭函式中 app 是一個參數
 
@@ -11,5 +12,9 @@ module.exports = app => { // 此箭頭函式中 app 是一個參數
 
   // 在 /admin/restaurants 底下則交給 adminController.getRestaurants 處理
   app.get('/admin/restaurants', adminController.getRestaurants)
+
+
+  app.get('/signup', userController.signUpPage)
+  app.post('/signup', userController.signUp)
 }
 
