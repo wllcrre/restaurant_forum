b835e2926ceb720d25801317ab397ebe27769271
kevin <keebvhev@gmail.com>
2019-10-08 11:53:50 +0800

    user register check use flash message

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const flash = require('connect-flash')
+const session = require('express-session')
+
 
 const db = require('./models') // 引入資料庫
 const bodyParser = require('body-parser') // for http POST, req.body 
+
+
 const app = express()
 const port = 3000
+
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+
+// 把 req.flash 放到 res.locals 裡面(這樣就不用每一個 controller 都要設定 flash messages)
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' })) //handlebars v3.1.0 優化中已直接帶入，故可不寫
 app.set('view engine', 'handlebars')
