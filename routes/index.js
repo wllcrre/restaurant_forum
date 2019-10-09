76b9a502d1b29164e548f29018d49442542bf773
kevin <keebvhev@gmail.com>
2019-10-09 11:51:06 +0800

    check login status and is user or admin

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,27 @@ const userController = require('../controllers/userController.js')
 
 module.exports = (app, passport) => { // 記得這邊要接收 passport
 
-  app.get('/', (req, res) => res.redirect('/restaurants'))
-  app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
+  const authenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    res.redirect('/signin')
+  }
+  const authenticatedAdmin = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      if (req.user.isAdmin) { return next() }
+      return res.redirect('/')
+    }
+    res.redirect('/signin')
+  }
 
-  //在 /restaurants 底下則交給 restController.getRestaurants 來處理
-  app.get('/restaurants', restController.getRestaurants)
+  // 在 /restaurants 底下則交給 restController.getRestaurants 來處理
+  app.get('/', authenticated, (req, res) => res.redirect('/restaurants'))
+  app.get('/restaurants', authenticated, restController.getRestaurants)
 
   // 在 /admin/restaurants 底下則交給 adminController.getRestaurants 處理
-  app.get('/admin/restaurants', adminController.getRestaurants)
+  app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
+  app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
 
 
   app.get('/signup', userController.signUpPage)
