37a68d49a5b545f0ea544b1416d7dbf592d52066
Jack Sung <magicsung@gmail.com>
2019-10-20 21:06:51 +0800

    fix port

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const bodyParser = require('body-parser') // for http POST, req.body
 
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app.use(methodOverride('_method'))
 
