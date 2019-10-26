561af24b5b7871e3a6041c73335e4e211fcd6ae8
kevin <keebvhev@gmail.com>
2019-10-26 17:44:51 +0800

    frontend show restaurants by category

--- controllers/restController.js
+++ controllers/restController.js
@@ -3,13 +3,25 @@ const Restaurant = db.Restaurant
 const Category = db.Category
 let restController = {
   getRestaurants: (req, res) => {
-    Restaurant.findAll({ include: Category }).then(restaurants => {
+    let whereQuery = {}
+    let categoryId = ''
+    if (req.query.categoryId) {
+      categoryId = Number(req.query.categoryId)
+      whereQuery['CategoryId'] = categoryId
+    }
+
+    Restaurant.findAll({ include: Category, where: whereQuery }).then(restaurants => {
       const data = restaurants.map(r => ({
         ...r.dataValues,
         description: r.dataValues.description.substring(0, 50)
       }))
-      return res.render('restaurants', {
-        restaurants: data
+
+      Category.findAll().then(categories => { // 取出 categoies 
+        return res.render('restaurants', {
+          restaurants: data,
+          categories: categories,
+          categoryId: categoryId
+        })
       })
     })
   },
